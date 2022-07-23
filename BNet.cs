76968dbd3dd9e312e04c8259f5c5d49a60751d21
David Ziegelbauer <dziegelbauer@outlook.com>
2022-07-22 21:37:06 -0600

    Add project files.

---
+++ BNet.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace BNETAPI
+{
+    public enum Region
+    {
+        US,
+        EU,
+        KR,
+        TW,
+        CN
+    }
+
+    public static class BNet
+    {
+        static string ClientID = String.Empty;
+        static string ClientSecret = String.Empty;
+        static string APIToken = String.Empty;
+
+        public static void Initialize(string clientid, string secret)
+        {
+            ClientID = clientid;
+            ClientSecret = secret;
+        }
+
+        internal static string CallAPI(string endpoint)
+        {
+            if (ClientID == String.Empty || ClientSecret == String.Empty)
+                throw new Exception("Client not initialized");
+
+            using (HttpClient client = new())
+            {
+                client.BaseAddress = new Uri(BNet.GetAPIEndpoint(Region.US));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = client.GetAsync("data/wow/achievement-category/index");
+            }
+
+            return String.Empty;
+        }
+
+        internal static string GetAPIEndpoint(Region rg)
+        {
+            return rg switch
+            {
+                Region.US => "https://us.api.blizzard.com/",
+                Region.EU => "https://eu.api.blizzard.com/",
+                Region.KR => "https://kr.api.blizzard.com/",
+                Region.TW => "https://tw.api.blizzard.com/",
+                Region.CN => "https://gateway.battlenet.com.cn/",
+                _ => throw new Exception("Invalid Region")
+            };
+        }
+    }
+}
---
+++ WoW/WoW.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNETAPI.WoW
+{
+    public static class WoW
+    {
+        public static Dictionary<int,string> GetAchievementCategoryIndex()
+        {
+            Dictionary<int, string> result = new();
+            
+
+            throw new NotImplementedException();
+        }
+    }
+}
