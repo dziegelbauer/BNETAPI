6c964ff78490e85eab71c417c972dce45ab0a783
David Ziegelbauer <dziegelbauer@outlook.com>
2022-07-24 17:52:18 -0600

    Achievement pulls working

--- BNet.cs
+++ BNet.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System.Text;
+using System.Text.Json;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 
@@ -15,9 +17,10 @@ namespace BNETAPI
 
     public static class BNet
     {
-        static string ClientID = String.Empty;
-        static string ClientSecret = String.Empty;
+        static string ClientID = "4c129af2795342f283e3d3f98dfe406f";// String.Empty;
+        static string ClientSecret = "RjHozaAXDLSAJdghW1LWDNWNbcxiONRh"; // String.Empty;
         static string APIToken = String.Empty;
+        static DateTime? APITokenExpiration = null;
 
         public static void Initialize(string clientid, string secret)
         {
@@ -25,10 +28,37 @@ namespace BNETAPI
             ClientSecret = secret;
         }
 
-        internal static string CallAPI(string endpoint)
+        internal static async Task<string> CallAPI(string endpoint)
         {
             if (ClientID == String.Empty || ClientSecret == String.Empty)
                 throw new Exception("Client not initialized");
+
+            HttpResponseMessage response;
+
+            using (HttpClient client = new())
+            {
+                if (APIToken == String.Empty || APITokenExpiration is null || DateTime.Now.CompareTo(APITokenExpiration) > 0)
+                {
+                    client.BaseAddress = new Uri(BNet.GetOAuthEndpoint(Region.US));
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{BNet.ClientID}:{BNet.ClientSecret}")));
+                    var content = new FormUrlEncodedContent(new[]
+                    {
+                        new KeyValuePair<string, string>("grant_type", "client_credentials"),
+                    });
+                    content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded");
+
+                    response = await client.PostAsync("oauth/token", content);
+
+                    var TokenReply = await response.Content.ReadAsStringAsync();
+                    var OAuthToken = JsonSerializer.Deserialize<OAuth2Token>(TokenReply);
+
+                    APIToken = OAuthToken?.access_token;
+                    APITokenExpiration = DateTime.Now.AddSeconds(OAuthToken.expires_in);
+                }
+            }
 
             using (HttpClient client = new())
             {
@@ -36,11 +66,12 @@ namespace BNETAPI
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(
                     new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", APIToken);
 
-                var response = client.GetAsync("data/wow/achievement-category/index");
+                response = await client.GetAsync($"{endpoint}&access_token={APIToken}");
+
+                return await response.Content.ReadAsStringAsync();
             }
-
-            return String.Empty;
         }
 
         internal static string GetAPIEndpoint(Region rg)
@@ -51,6 +82,19 @@ namespace BNETAPI
                 Region.EU => "https://eu.api.blizzard.com/",
                 Region.KR => "https://kr.api.blizzard.com/",
                 Region.TW => "https://tw.api.blizzard.com/",
+                Region.CN => "https://gateway.battlenet.com.cn/",
+                _ => throw new Exception("Invalid Region")
+            };
+        }
+
+        internal static string GetOAuthEndpoint(Region rg)
+        {
+            return rg switch
+            {
+                Region.US => "https://us.battle.net/",
+                Region.EU => "https://eu.battle.net/",
+                Region.KR => "https://kr.battle.net/",
+                Region.TW => "https://tw.battle.net/",
                 Region.CN => "https://gateway.battlenet.com.cn/",
                 _ => throw new Exception("Invalid Region")
             };
--- WoW/WoW.cs
+++ WoW/WoW.cs
@@ -8,12 +8,56 @@ namespace BNETAPI.WoW
 {
     public static class WoW
     {
-        public static Dictionary<int,string> GetAchievementCategoryIndex()
+        public static async Task<Dictionary<int,string>> GetAchievementCategoryIndex()
         {
             Dictionary<int, string> result = new();
-            
+
+            var response = await BNet.CallAPI("data/wow/achievement-category/index?namespace=static-us");
 
             throw new NotImplementedException();
+        }
+
+        public static async Task<string> GetAchievementCategory(string id)
+        {
+            var response = await BNet.CallAPI($"data/wow/achievement-category/{id}?namespace=static-us");
+
+            return response;
+        }
+
+        public static async Task<string> GetAchievementCategory(int id)
+        {
+            return await GetAchievementCategory(id.ToString());
+        }
+
+        public static async Task<string> GetAchievementIndex()
+        {
+            var response = await BNet.CallAPI("data/wow/achievement/index?namespace=static-us");
+
+            return response;
+        }
+
+        public static async Task<string> GetAchievement(string id)
+        {
+            var response = await BNet.CallAPI($"data/wow/achievement/{id}?namespace=static-us");
+
+            return response;
+        }
+
+        public static async Task<string> GetAchievement(int id)
+        {
+            return await GetAchievement(id.ToString());
+        }
+
+        public static async Task<string> GetAchievementMedia(string id)
+        {
+            var response = await BNet.CallAPI($"data/wow/media/achievement/{id}?namespace=static-us");
+
+            return response;
+        }
+
+        public static async Task<string> GetAchievementMedia(int id)
+        {
+            return await GetAchievementMedia(id.ToString());
         }
     }
 }
